a363aca0161c8939df70f93d18a123ed086971f8
arnas-bradauskas <119974036+arnas-bradauskas@users.noreply.github.com>
2022-12-06 23:37:52 +0200

    Add files via upload

---
+++ Branches.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Branches
+{
+    public class Branch
+    {
+        public char Letter { get; set; }
+        private List<Branch> branches;
+
+        public Branch(char letter)
+        {
+            Letter = letter;
+            branches = new List<Branch>();
+        }
+
+        public Branch GetBranch(int index) { return branches[index]; }
+
+        public void Add(Branch branch) { branches.Add(branch); }
+
+        public int Count() { return branches.Count; }
+    }
+
+    class Branches
+    {
+        static int MaxHeight(Branch branch)
+        {
+            int height = 1;
+
+            if (branch.Count() == 0) return height;
+
+            for(int i = 0; i < branch.Count(); i++)
+            {
+                height = Math.Max(height, MaxHeight(branch.GetBranch(i)));
+            }
+
+            return height+1;
+        }
+
+        //                       A
+        //
+        //                     /   \
+        //
+        //                   B       C
+        //
+        //                 /      /  |  \
+        //
+        //               D      E    F    G
+        //
+        //                    /    /   \    \
+        //
+        //                  H     I     J     K
+        //
+        //                        |
+        //
+        //                        L
+
+        static void Main(string[] args)
+        {
+            Branch branch = new Branch('A');
+            branch.Add(new Branch('B'));
+            branch.Add(new Branch('C'));
+            branch.GetBranch(0).Add(new Branch('D'));
+            branch.GetBranch(1).Add(new Branch('E'));
+            branch.GetBranch(1).Add(new Branch('F'));
+            branch.GetBranch(1).Add(new Branch('G'));
+            branch.GetBranch(1).GetBranch(0).Add(new Branch('H'));
+            branch.GetBranch(1).GetBranch(1).Add(new Branch('I'));
+            branch.GetBranch(1).GetBranch(1).Add(new Branch('J'));
+            branch.GetBranch(1).GetBranch(2).Add(new Branch('K'));
+            branch.GetBranch(1).GetBranch(1).GetBranch(0).Add(new Branch('L'));
+
+            Console.WriteLine("Depth of provided structure: " + MaxHeight(branch));
+        }
+    }
+}
---
+++ Clock.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Clock
+{
+    class Clock
+    {
+        static int calcAngle(int hours, int minutes)
+        {
+            //Check if negative
+            if (hours < 0 || minutes < 0)
+                Console.Write("Hours and minutes must be positive");
+
+            //Convert any extra minutes into hours
+            if (minutes >= 60)
+            {
+                hours += minutes / 60;
+                minutes %= 60;
+            }
+
+            // Calculate the angles moved by hour and minute hands from 00:00
+            int hourAngle = (int)(0.5 * (hours * 60 + minutes));
+            int minuteAngle = (int)(6 * minutes);
+
+            // Find the difference between two angles
+            int angle = Math.Abs(hourAngle - minuteAngle);
+
+            // Find smaller angle of two possible angles
+            angle = Math.Min(360 - angle, angle);
+
+            return angle;
+        }
+
+        static void Main(string[] args)
+        {
+            //The program will work with any positive integer for both hours and minutes
+            Console.WriteLine("Input hours");
+            int hours = int.Parse(Console.ReadLine()) % 12;
+
+            Console.WriteLine("Input minutes");
+            int minutes = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Angle: " + calcAngle(hours, minutes));
+        }
+    }
+}
